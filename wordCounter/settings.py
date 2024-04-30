6d53487b4771e2c10dec327cdd59250c2c51a69b
tugrulvural89 <tugrulv89@gmail.com>
2024-05-01 01:20:42 +0300

    v1.0.0

--- wordCounter/settings.py
+++ wordCounter/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['counterwizard.com']
+ALLOWED_HOSTS = ['www.counterwizard.com']
 
 
 # Application definition
